caa48b64c8fb60fecee1135b7d661441f860e54f
michaels <radpangolin@gmail.com>
2017-08-28 22:40:03 +0100

    added icons, improved messaging layout

--- app/src/main/java/io/github/idoqo/pangolin/MessageActivity.java
+++ app/src/main/java/io/github/idoqo/pangolin/MessageActivity.java
@@ -3,7 +3,12 @@ package io.github.idoqo.pangolin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.net.URL;
+import java.net.URLConnection;
+
 public class MessageActivity extends AppCompatActivity {
+    private static final String URL_HNGFUN_PROFILE = "http://hng.fun/profile/pangolin.php";
+    private static final String KEY_LOCATION_HEADER = "location";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
